b4b5a58c950b3109acdc5e02d18b6277c193a0b8
SalmanShaikh3223 <shaikh.sal@northeastern.edu>
2023-11-26 18:46:40 -0500

    final commit

---
+++ Lab_7_/Lab_7/src/business/Organization/LabOrganization.java
@@ -0,0 +1,24 @@
+package business.Organization;
+
+import business.Role.LabAssistantRole;
+import business.Role.Role;
+import java.util.ArrayList;
+
+
+public class LabOrganization extends Organization{
+
+    public LabOrganization() {
+        super(Organization.Type.Lab.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new LabAssistantRole());
+        return roles;
+    }
+     
+   
+    
+    
+}
---
+++ Lab_7_/Lab_7/src/business/Role/Role.java
@@ -0,0 +1,40 @@
+package business.Role;
+
+import business.Business;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+public abstract class Role {
+
+    public enum RoleType {
+        Admin("Admin"),
+        Doctor("Doctor"),
+        LabAssistant("Lab Assistant");
+
+        private String value;
+
+        private RoleType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    public RoleType type;
+
+    public abstract JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Business business);
+
+    @Override
+    public String toString() {
+        return (type != null) ? this.type.getValue() : this.getClass().getName();
+    }
+
+}
